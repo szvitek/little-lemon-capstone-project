bc2ce602b4a981adf5207b6074a139a12e13f1f0
szvitek <gy.szvitek@gmail.com>
2024-07-12 12:56:53 +0100

    feat(Components): finish styling the components

--- src/components/BookingForm.js
+++ src/components/BookingForm.js
@@ -20,55 +20,48 @@ export default function BookingForm({ availableTimes }) {
   return (
     <form className="booking-form" onSubmit={handleSubmit}>
       <h2>Book Now</h2>
-      <div>
-        <label htmlFor="res-date">Choose date</label>
-        <input
-          type="date"
-          id="res-date"
-          value={date}
-          onChange={(e) => setDate(e.target.value)}
-        />
-      </div>
 
-      <div>
-        <label htmlFor="res-time">Choose time</label>
-        <select
-          id="res-time"
-          value={time}
-          onChange={(e) => setTime(e.target.value)}
-        >
-          {availableTimes.map((time) => (
-            <option key={time} value={time}>
-              {time}
-            </option>
-          ))}
-        </select>
-      </div>
+      <label htmlFor="res-date">Choose date</label>
+      <input
+        type="date"
+        id="res-date"
+        value={date}
+        onChange={(e) => setDate(e.target.value)}
+      />
 
-      <div>
-        <label htmlFor="guests">Number of guests</label>
-        <input
-          type="number"
-          placeholder="1"
-          min="1"
-          max="10"
-          id="guests"
-          value={guests}
-          onChange={(e) => setGuests(e.target.value)}
-        />
-      </div>
+      <label htmlFor="res-time">Choose time</label>
+      <select
+        id="res-time"
+        value={time}
+        onChange={(e) => setTime(e.target.value)}
+      >
+        {availableTimes.map((time) => (
+          <option key={time} value={time}>
+            {time}
+          </option>
+        ))}
+      </select>
 
-      <div>
-        <label htmlFor="occasion">Occasion</label>
-        <select
-          id="occasion"
-          value={occasion}
-          onChange={(e) => setOccasion(e.target.value)}
-        >
-          <option value="birthday">Birthday</option>
-          <option value="anniversary">Anniversary</option>
-        </select>
-      </div>
+      <label htmlFor="guests">Number of guests</label>
+      <input
+        type="number"
+        placeholder="1"
+        min="1"
+        max="10"
+        id="guests"
+        value={guests}
+        onChange={(e) => setGuests(e.target.value)}
+      />
+
+      <label htmlFor="occasion">Occasion</label>
+      <select
+        id="occasion"
+        value={occasion}
+        onChange={(e) => setOccasion(e.target.value)}
+      >
+        <option value="birthday">Birthday</option>
+        <option value="anniversary">Anniversary</option>
+      </select>
 
       <input type="submit" value="Make Your reservation"></input>
     </form>
--- src/components/CustomersSay.js
+++ src/components/CustomersSay.js
@@ -1,17 +1,55 @@
+const feedbacks = [
+  {
+    name: 'Oliver',
+    img: 'https://unsplash.com/photos/6anudmpILw4/download?ixid=M3wxMjA3fDB8MXxzZWFyY2h8Mzh8fHBvcnRyYWl0fGVufDB8fHx8MTcyMDc3NTE3OXww&force=true&w=640',
+    rating: 5,
+    text: 'Best pizzas in town',
+  },
+  {
+    name: 'Sofia',
+    img: 'https://unsplash.com/photos/IF9TK5Uy-KI/download?ixid=M3wxMjA3fDB8MXxhbGx8fHx8fHx8fHwxNzIwNzc3OTg3fA&force=true&w=640',
+    rating: 5,
+    text: 'Little Lemon staff always go above and beyond. Quality food and professional serving.',
+  },
+  {
+    name: 'Hugo',
+    img: 'https://unsplash.com/photos/ILip77SbmOE/download?ixid=M3wxMjA3fDB8MXxhbGx8fHx8fHx8fHwxNzIwNzY2NjUxfA&force=true&w=640',
+    rating: 4,
+
+    text: 'Food is so good but the place is always crowded, the dining area could be bigger',
+  },
+];
+
+function CustomerCard({ name, img, rating, text }) {
+  return (
+    <div className="customer-card">
+      <img className="customer-card__image" src={img} alt="customer" />
+      <div className="customer-card__details">
+        <h4 className="customer-card__name">{name}</h4>
+        <span className="customer-card__rating">5/{rating}</span>
+        <p className="customer-card__text">{text}</p>
+      </div>
+    </div>
+  );
+}
+
 export default function CustomersSay() {
   return (
     <section className="testimonials">
       <div class="testimonials__container">
-        <h3>Testimonials</h3>
+        <h2>Testimonials</h2>
+        
         {/* cards container */}
         <div className="testimonials__cards">
-          {/* card *3 */}
-          <div>
-            <h4>Name</h4>
-            <img src="" alt="" />
-            <span>Rating</span>
-            <p>this user said this dish is amazing</p>
-          </div>
+          {feedbacks.map((feedback) => (
+            <CustomerCard
+              key={feedback.name}
+              name={feedback.name}
+              img={feedback.img}
+              rating={feedback.rating}
+              text={feedback.text}
+            />
+          ))}
         </div>
       </div>
     </section>
